3503933b071a23ca3e4992d249e718c64c6349e1
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-09-17 23:51:11 -0500

    first commit

---
+++ src/tutorial/Main.java
@@ -0,0 +1,57 @@
+package tutorial;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		//primitive data types
+//		int helloWorld=9;
+//		double num2=5.0;
+//		boolean b=true;
+//		char c='t';
+//		
+//		//non-primitive data types
+//		String str="tim";
+//		
+//		int tim=helloWorld;
+//		int x;
+//		x=5;
+//		int y=7;
+//		int z=57;
+//		int sum=x+y+z;
+//		int u =z/y;
+//		double d=Math.pow(x, y);
+//		double div=x/(double)y;
+//		
+//		System.out.println("Hello World!");
+//		System.out.println(helloWorld+num2);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(str);
+//		System.out.println(tim);
+//		System.out.println(sum);
+//		System.out.println(u);
+//		System.out.println(d);
+//		System.out.println(div);	
+//		int x= 56 % 5;
+//		System.out.println(x);
+		
+		int a=6;
+		int b=23;
+		int c=10;
+		// > < == >= <= !=
+		boolean compare= a > b || c < b;
+		System.out.println(compare);
+		
+//		
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("enter an integer");
+//		String scanned=sc.next();
+//		int x= Integer.parseInt(scanned);
+//		
+//		System.out.println(x);
+		
+	}
+
+}
