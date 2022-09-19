ee2c5be34fe4fbf3a705dfadb8079a603afe89ca
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-09-18 23:15:41 -0500

    learned about arr and loop syntax in java

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
 //		//primitive data types
 //		int helloWorld=9;
 //		double num2=5.0;
 //		boolean b=true;
 //		char c='t';
-//		
 //		//non-primitive data types
 //		String str="tim";
-//		
 //		int tim=helloWorld;
 //		int x;
 //		x=5;
@@ -23,7 +20,6 @@ public class Main {
 //		int u =z/y;
 //		double d=Math.pow(x, y);
 //		double div=x/(double)y;
-//		
 //		System.out.println("Hello World!");
 //		System.out.println(helloWorld+num2);
 //		System.out.println(b);
@@ -36,21 +32,132 @@ public class Main {
 //		System.out.println(div);	
 //		int x= 56 % 5;
 //		System.out.println(x);
-		
-		int a=6;
-		int b=23;
-		int c=10;
+//		int a=6;
+//		int b=23;
+//		int c=10;
 		// > < == >= <= !=
-		boolean compare= a > b || c < b;
-		System.out.println(compare);
-		
-//		
+		// check is strings are equal: string1.equals("string2")
+//		boolean compare= a > b || c < b;
+//		System.out.println(compare);
 //		Scanner sc= new Scanner(System.in);
 //		System.out.println("enter an integer");
 //		String scanned=sc.next();
 //		int x= Integer.parseInt(scanned);
-//		
 //		System.out.println(x);
+		
+		
+//		Scanner sc= new Scanner(System.in);
+//		String s=sc.nextLine();
+//		if(s.equals("tim")) {
+//			System.out.println("You typed tim");
+//		}else if(s.equals("bob")){
+//			System.out.println("You typed bob");
+//		} else {
+//			System.out.println("You didn't type tim or bob");
+//		}
+//		System.out.println("End of Program");
+		
+//		System.out.println("Enter your age: ");
+//		Scanner sc= new Scanner(System.in);
+//		String s=sc.nextLine();
+//		int age=Integer.parseInt(s);
+//		if(age>=18) {
+//			System.out.println("input your favorite food:");
+//			String food=sc.nextLine();
+//			if(food.equals("pizza")) {
+//				System.out.println("mine too");
+//			}else {
+//				System.out.println("not mine");
+//			}
+//			System.out.println("you are an adult");
+//		}else if(age>=13){
+//			System.out.println("you are a teenager");
+//		}else {
+//			System.out.println("you are not a teenager or an adult");
+//		}
+		
+		//you should declare the number you want to enter into the array
+		//put the type of arr you want followed by square brackets to initialize
+//		int[] newArr=new int[5];
+//		String[] strArr=new String[5];
+//		
+//		strArr[0]="hello";
+//		strArr[1]="hi";
+//		strArr[2]="tim";
+//		strArr[3]="bill";
+//		strArr[4]="joe";
+//		
+//		int[] nums= {2,3,54,6,6};
+//		double[] nums2= {2.0,3.0};
+//		
+//		int x= nums[4];
+//		System.out.println(x);
+		
+		
+		//for loops
+//		int x=0;
+//		int[] arr= {1,5,7,3,4,5};
+//		for(int i=0;i<arr.length;i++) {
+//			x+=i;
+//			if(arr[i]==5) {
+//				System.out.println("Found a 5! at index "+i);
+//			}
+//		}
+//		System.out.println(x);
+		
+		
+		//for each loop
+		//for int elements inside the array called arr
+//		int [] arr= {1,5,7,3,4,5};
+//		int count=0;
+//		for(int element:arr) {
+//			System.out.println(element+" "+count);
+//			count++;
+//		}
+		
+//		String[] names= new String[5];
+//		Scanner sc= new Scanner(System.in);
+//		for(int i=0;i<names.length;i++) {
+//			System.out.print("Input name:");
+//			String input=sc.nextLine();
+//			names[i]=input;
+//		}
+//		System.out.println("");
+//		System.out.println("Names inputted:");
+//		for(String name:names) {
+//			System.out.println(name);
+//			if(name.equals("tim")) {
+//				System.out.println("Encountered Tim. Breaking loop");
+//				break;
+//			}
+//		}
+		
+		//while loops
+//		int counter=0;
+//		while(counter<5) {
+//			System.out.println("count: " + counter);
+//			counter++;
+//		}
+		
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("Type 10...");
+//		System.out.print("Type a number: ");
+//		int x = sc.nextInt();
+//		while(x!=10) {
+//			System.out.println("Type 10...");
+//			System.out.print("Type a number: ");
+//			x=sc.nextInt();
+//		}
+		
+		//do while loop
+//		Scanner sc= new Scanner(System.in);
+//		int x;
+//		do {
+//			System.out.println("Type 10...");
+//			System.out.print("Type a number: ");
+//			x=sc.nextInt();
+//		}while(x!=10);
+
 		
 	}
 
